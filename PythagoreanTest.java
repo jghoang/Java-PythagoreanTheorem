dbb714448898085a9f8ea3dd3f4fd279883f9a55
jghoang <50341293+jghoang@users.noreply.github.com>
2019-06-16 13:07:55 -0500

    Update PythagoreanTest.java

--- PythagoreanTest.java
+++ PythagoreanTest.java
@@ -1,7 +1,7 @@
 public class PythagoreanTest {
     public static void main (String[] args){
         Pythagorean iD = new Pythagorean();
-        double calculate = iD.calculateHypotenuse(10, 15);
+        double calculate = iD.calculateHypotenuse(10, 15); //You can change the vaules to calculate the hypotenuse
         System.out.println(calculate);
     }
 }
