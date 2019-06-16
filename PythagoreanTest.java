4091a7ad6d138cc1ce33bf90dc835a78754f3073
jghoang <50341293+jghoang@users.noreply.github.com>
2019-06-16 13:06:51 -0500

    Add files via upload

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main (String[] args){
+        Pythagorean iD = new Pythagorean();
+        double calculate = iD.calculateHypotenuse(10, 15);
+        System.out.println(calculate);
+    }
+}
